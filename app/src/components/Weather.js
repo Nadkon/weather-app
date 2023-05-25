5d8582c644c13a8f453ab6350efb2c669c7d1e54
nadkon <nadkon@gmail.com>
2023-05-25 20:51:58 +0300

    component Weather updated: added object inside the function

--- app/src/components/Weather.js
+++ app/src/components/Weather.js
@@ -1,30 +1,40 @@
 import React from "react";
 
 export default function Weather() {
+  let weatherData = {
+    city: "Kyiv",
+    date: "25 May 2023",
+    description: "Sunny",
+    humidity: 20,
+    temperature: +25,
+    windSpeed: 1,
+    imageUrl:'',
+  }
+
   return (
     <div className="weather-info">
       <div className="row">
         <div className="col-6">
-          <h1 className="city"> Kyiv</h1>
+          <h1 className="city"> {weatherData.city}</h1>
           <ul>
             <li>
               Last updated:
-              <span className="date">25 May 2023</span>
+              <span className="date">{weatherData.date}</span>
             </li>
             <li>
-              <span className="description">Sunny</span>
+              <span className="description">{weatherData.description}</span>
             </li>
             <li>
               <ul>
                 <li>
                   Humidity:
                   <strong>
-                    <span className="humidity">20%</span>%
+                    <span className="humidity">{weatherData.humidity}%</span>%
                   </strong>
                 </li>
                 Wind:
                 <strong className="lowercase">
-                  <span>1</span>m/h
+                  <span>{weatherData.windSpeed}</span>m/h
                 </strong>
               </ul>
             </li>
@@ -32,9 +42,9 @@ export default function Weather() {
         </div>
         <div className="col-6">
           <div className="temperature-container d-flex justify-content-end">
-            <img src="" alt="" />
+            <img src={weatherData.imageUrl} alt={weatherData.description} />
             <span className="temperature"></span>
-            <span className="unit">+25°C</span>
+            <span className="unit">{weatherData.temperature}°C</span>
           </div>
         </div>
       </div>
