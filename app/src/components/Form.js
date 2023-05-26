73998e177d289b14c06290f1c06d3c8998051da6
nadkon <nadkon@gmail.com>
2023-05-26 15:22:54 +0300

    fix: form design updated, the current location button deleted

--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -4,7 +4,7 @@ export default function Form() {
   return (
     <form className="mb-2">
       <div className="row">
-        <div className="col-5">
+        <div className="col-8">
           <input
             type="search"
             placeholder="Type a city name..."
@@ -12,18 +12,11 @@ export default function Form() {
             autoComplete="off"
           />
         </div>
-        <div className="col-3">
+        <div className="col-4">
           <input
             type="submit"
             value="Search"
             className="btn btn-primary w-100"
-          />
-        </div>
-        <div className="col-3">
-          <input
-            type="submit"
-            value="Current location"
-            className="btn btn-info w-30 current-location-btn"
           />
         </div>
       </div>
