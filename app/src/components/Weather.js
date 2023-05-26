a73c08b78edb4a2083dff0f67be9fcfe8aea4486
nadkon <nadkon@gmail.com>
2023-05-26 14:35:48 +0300

    feat: api added to weather component

--- app/src/components/Weather.js
+++ app/src/components/Weather.js
@@ -1,53 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Weather() {
-  let weatherData = {
-    city: "Kyiv",
-    date: "25 May 2023",
-    description: "Sunny",
-    humidity: 20,
-    temperature: +25,
-    windSpeed: 1,
-    imageUrl:'',
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+
+  function handleResponse(response) {
+    console.log(response.data);
+    console.log(response.data.weather[0].main);
+
+    setWeatherData({
+      city: response.data.name,
+      date: "25 May 2023",
+      description: response.data.weather[0].main,
+      humidity: response.data.main.humidity,
+      temperature: response.data.main.temp,
+      windSpeed: response.data.wind.speed,
+      imageUrl: "",
+    });
+
+    setReady(true);
   }
 
-  return (
-    <div className="weather-info">
-      <div className="row">
-        <div className="col-6">
-          <h1 className="city"> {weatherData.city}</h1>
-          <ul>
-            <li>
-              Last updated:
-              <span className="date">{weatherData.date}</span>
-            </li>
-            <li>
-              <span className="description">{weatherData.description}</span>
-            </li>
-            <li>
-              <ul>
-                <li>
-                  Humidity:
-                  <strong>
-                    <span className="humidity">{weatherData.humidity}%</span>%
+  if (ready) {
+    return (
+      <div className="weather-info">
+        <div className="row">
+          <div className="col-6">
+            <h1 className="city"> {weatherData.city}</h1>
+            <ul>
+              <li>
+                Last updated:
+                <span className="date">{weatherData.date}</span>
+              </li>
+              <li>
+                <span className="description">{weatherData.description}</span>
+              </li>
+              <li>
+                <ul>
+                  <li>
+                    Humidity:
+                    <strong>
+                      <span className="humidity">{weatherData.humidity}</span>%
+                    </strong>
+                  </li>
+                  Wind:
+                  <strong className="lowercase">
+                    <span>{weatherData.windSpeed}</span>m/h
                   </strong>
-                </li>
-                Wind:
-                <strong className="lowercase">
-                  <span>{weatherData.windSpeed}</span>m/h
-                </strong>
-              </ul>
-            </li>
-          </ul>
-        </div>
-        <div className="col-6">
-          <div className="temperature-container d-flex justify-content-end">
-            <img src={weatherData.imageUrl} alt={weatherData.description} />
-            <span className="temperature"></span>
-            <span className="unit">{weatherData.temperature}°C</span>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div className="col-6">
+            <div className="temperature-container d-flex justify-content-end">
+              <img src={weatherData.imageUrl} alt={weatherData.description} />
+              <span className="temperature"></span>
+              <span className="unit">
+                {Math.round(weatherData.temperature)}°C
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = "Kyiv";
+    let unit = "metric";
+    const apiKey = "be60748992fab0f5da8162563fb21245";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading....";
+  }
 }
